8d0ce5e7046037a9faaa392e54b5810f1aad32b5
Beatriz Salles <beatrizsallesss@gmail.com>
2022-01-15 11:44:18 -0300

    chore: Add favicon

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import { Box, Text } from "@chakra-ui/react";
 
 export default function Home () {
   return (
-    <Box>
+    <Box  w='100%'  bgGradient='linear(to-r, green.200, pink.500)'>
       <Text>Home</Text>
     </Box>
   )
